b30955539022297614d56d4c15968bcf08b674ae
jsong73 <jessicajinsong@gmail.com>
2024-05-05 22:38:21 -0400

    closing dropdown when outside dropdown is clicked

--- client/src/components/DeleteBtn.jsx
+++ client/src/components/DeleteBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect, useRef  } from "react";
 import { CiMenuKebab } from "react-icons/ci";
 import Auth from "../../utils/auth"
 import { deleteSubtask, deleteTask , updateTask} from "../../utils/api";
@@ -10,9 +10,9 @@ const token = Auth.getToken();
 // console.log("is taskID being passed down?", taskId)
 // console.log("is subtaskID being passed down?", subtaskId)
   const [showModal, setModal] = useState(false);
+  const menuRef = useRef(null); 
 
-  const handleToggleMenu = (event) => {
-    event.stopPropagation(); 
+  const handleSelectMenu = () => {
     setModal(true);
   };
 
@@ -56,13 +56,29 @@ const token = Auth.getToken();
     return null;
   };
 
+
+  //close menu when clicking outside of the drop down menu
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
   return (
     <div className="relative">
-      <button onClick={handleToggleMenu} className="absolute top-0 right-0">
+      <button onClick={handleSelectMenu} className="absolute top-0 right-0">
         <CiMenuKebab />
       </button>
       {showModal && (
-        <div className="absolute right-0 top-8 bg-white border border-gray-200 rounded-md shadow-md z-10 w-36 ">
+        <div className="absolute right-0 top-8 bg-white border border-gray-200 rounded-md shadow-md z-10 w-36 " ref={menuRef}>
           <ul>
             <li className="py-1 px-3 hover:bg-gray-100 cursor-pointer" onClick={handleClick}>
               Delete
