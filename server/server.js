d665cee08ce76628f8966b5c3834fe1a5990deac
jsong73 <jessicajinsong@gmail.com>
2024-05-10 17:36:01 -0400

    added path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors")
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
