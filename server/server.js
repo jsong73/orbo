d54c3a50f4bf4a5fe17cbdeac4b7be88f39fd42c
jsong73 <jessicajinsong@gmail.com>
2024-05-10 16:58:29 -0400

    added deployment lines

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ const subtaskRoutes = require("./routes/subtaskRoutes")
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+    
+
 app.use("/", userRoutes)
 app.use("/tasks", taskRoutes)
 app.use("/tasks", subtaskRoutes)
