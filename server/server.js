1a9da11ef1f9a1b9d5720577b43e5d9419bcd175
jsong73 <jessicajinsong@gmail.com>
2024-05-13 14:29:25 -0400

    forgot to add in path

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors")
-// const path = require("path");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
