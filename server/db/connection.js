a857d6a567fa445a4a21d2445a86169d6161d531
jsong73 <jessicajinsong@gmail.com>
2024-05-13 22:37:40 -0400

    fixed host

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
-    host: "localhost",
+    host: "https://orbo.onrender.com",
     port: 5432,
     database: "tasks_db"
 })
