c958b06aa3b8656ae4ad17a88ceb1d7cde189372
jsong73 <jessicajinsong@gmail.com>
2024-05-21 16:20:21 -0400

    adding host

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
-    host: "127.0.0.1",
+    host:  process.env.DB_HOST,
     port: 5432,
     database: "tasks_db",
     ssl: {rejectUnauthorized: false}
