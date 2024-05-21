f9adec77aec9b1390bf495e624e79f3eebae6cd4
jsong73 <jessicajinsong@gmail.com>
2024-05-21 16:38:21 -0400

    debug

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,17 @@
 const { Pool } = require("pg");
 
+console.log('DB Config:', {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: "127.0.0.1",
+    port: 5432,
+    database: "tasks_db",
+});
+
 const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
-    host:  process.env.DB_HOST,
+    host: "127.0.0.1",
     port: 5432,
     database: "tasks_db",
     ssl: {rejectUnauthorized: false}
@@ -13,9 +21,9 @@ pool.on("connect", () => {
     console.log("connected to the task_db database");
 });
 
-pool.connect((err) => {
-    if (err) throw err
-    console.log("connected to PostgreSQL successfully")
-})
+pool.on("error", (err) => {
+    console.error("error on idle client', err");
+    process.exit(-1);
+});
 
 module.exports = { pool };
