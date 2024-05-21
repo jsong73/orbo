b1a08151be6ff2173a861f90aa0ed9ce5bfea6fb
jsong73 <jessicajinsong@gmail.com>
2024-05-21 14:41:29 -0400

    fixed url

--- client/utils/api.jsx
+++ client/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-    // const baseURL= "http://orbo.onrender.com";
-   const baseURL= "http://localhost:3001";
+    const baseURL= "http://orbo.onrender.com";
+//    const baseURL= "http://localhost:3001";
    
     export const login = async (data) => {
         try {
